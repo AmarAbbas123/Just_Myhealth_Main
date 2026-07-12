4c5430f52da734e1fb4d65d947202bf33cf6fba6
Amar Abbas <amarabbas1414@gmail.com>
2026-07-12 21:30:53 +0500

    now only show 3 recent post

--- app/Http/Controllers/Modules/Mod01SystemAdministration/Blog/BlogController.php
+++ app/Http/Controllers/Modules/Mod01SystemAdministration/Blog/BlogController.php
@@ -33,7 +33,7 @@ class BlogController extends Controller
         $related = BlogPost::published()
             ->where('id', '!=', $blogPost->id)
             ->orderByDesc('PublishedAt')
-            ->take(5)
+            ->take(3)
             ->get();
 
         return view('modules.mod-ps.general.blog-show', compact('blogPost', 'related'));
